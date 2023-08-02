c5b6370c10d8def778c93fb12f674e4bba471859
Nhu-Nguyen <121688890+nhunguyen-debug@users.noreply.github.com>
2023-08-01 21:45:42 -0500

    update password mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://wren:<FyYSIWfGDvMbtk7N>@cluster0.ajgtrzz.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://wren:<landung123>@cluster0.ajgtrzz.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
