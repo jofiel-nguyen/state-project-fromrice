a7eaec2ccb5cfac4a45c2b4229d9c577dfa6d2d8
Nhu-Nguyen <121688890+nhunguyen-debug@users.noreply.github.com>
2023-08-01 22:31:34 -0500

    change link mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://wren:manymany123@cluster0.ajgtrzz.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect("mongodb://wren:manymany123@ac-ehvxfhn-shard-00-00.7vn0qad.mongodb.net:27017,ac-ehvxfhn-shard-00-01.7vn0qad.mongodb.net:27017,ac-ehvxfhn-shard-00-02.7vn0qad.mongodb.net:27017/?ssl=true&replicaSet=atlas-mma197-shard-0&authSource=admin&retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
