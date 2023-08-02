c4aab84d978fbcaa2ab75edae8eec19436d849de
Nhu-Nguyen <121688890+nhunguyen-debug@users.noreply.github.com>
2023-08-01 22:16:58 -0500

    update pass

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://wren:<Tunhu123@@>@cluster1.7vn0qad.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://wren:manymany123@cluster0.ajgtrzz.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
